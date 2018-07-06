0bd314b58b46fb6e370ed52f59db69c6cd45b5ed
Mauricio Ferreyra (VPN) <mauricio.ferreyra@venhapranuvem.com.br>
2018-07-05 23:41:30 -0300

    commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './app.css';
 
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+
+
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -16,6 +20,12 @@ export default class App extends Component {
   render() {
     return (
       <div>
+      <Fabric>
+      <DefaultButton>
+        I am a button.
+      </DefaultButton>
+    </Fabric>
+
         {this.state.username ? (
           <h1>Hello {this.state.username}</h1>
         ) : (
